5035067d19e243015956ea20dfb1307e67ff2afe
Otabek <otabekchorshanbiyev5161@gnail.com>
2025-07-09 18:34:05 +0500

    update API base URL to HTTPS and rename Firebase hosting workflow for Flutter web deployment

--- lib/service/api_service.dart
+++ lib/service/api_service.dart
@@ -10,7 +10,7 @@ class ApiService {
   ApiService._internal() {
     dio = Dio(
       BaseOptions(
-        baseUrl: 'http://otash08.uz/api/',
+        baseUrl: 'https://otash08.uz/api/',
         connectTimeout: const Duration(seconds: 10),
         receiveTimeout: const Duration(seconds: 10),
         headers: {
